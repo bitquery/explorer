e2d441e2da4340fe5ce6172fb3e4de57b87c5f47
Andrey <bigzero@yandex.ru>
2019-12-16 17:54:36 +0300

    даты фиксы

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,23 +3,92 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 import '../stylesheets/application'
-// import "@fortawesome/fontawesome-free/js/all";
-// require("@rails/ujs").start()
+require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-// import $ from 'jquery'
 require("popper.js")
 require("bootstrap")
 require("widgets/dist/widgets")
+require('daterangepicker')
 import widgetsGraphiql from 'widgets/dist/widgetsGraphiql'
 import widgetsGraph from 'widgets/dist/widgetsGraph'
-console.log('10');
+import moment from 'moment'
+console.log('33');
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+global.reportRange = function(selector, from, till, i18n){
+    var properties = {
+        start: undefined,
+        end: undefined,
+        i18n: i18n,
+        ranges: {}
+    };
+
+    if (i18n.locale == 'ru'){
+        moment.locale('ru');
+    } else if ((i18n.locale == 'zh')){
+        moment.locale('zh-cn');
+    } else {
+        moment.locale('en');
+    }
+
+    if (from == '' && till == ''){
+        $(selector).find('span').html(i18n.all_time);
+    } else {
+        properties.start = moment(from);
+        properties.end = moment(till);
+    }
+
+    function set_reportrange(start, end) {
+        $(selector).find('span').html(start.format(i18n.format) + ' - ' + end.format(i18n.format));
+    }
+
+    properties.ranges[i18n.today] = [moment(), moment()];
+    properties.ranges[i18n.yesterday] = [moment().subtract(1, 'days'), moment().subtract(1, 'days')];
+    properties.ranges[i18n.last7] = [moment().subtract(6, 'days'), moment()];
+    properties.ranges[i18n.last30] = [moment().subtract(29, 'days'), moment()];
+    properties.ranges[i18n.this_month] = [moment().startOf('month'), moment().endOf('month')];
+    properties.ranges[i18n.last_month] = [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')];
+
+    $(selector).daterangepicker({
+        startDate: properties.start,
+        endDate: properties.end,
+        opens: 'left',
+        alwaysShowCalendars: true,
+        locale: {
+            cancelLabel: i18n.clear,
+            applyLabel: i18n.apply,
+            customRangeLabel: i18n.custom_range
+        },
+        ranges: properties.ranges
+    }, set_reportrange);
+
+    $(selector).on('cancel.daterangepicker', function(ev, picker) {
+        $(selector).find('span').html(i18n.all_time);
+    });
+
+    if (from != '' && till != ''){
+        set_reportrange(properties.start, properties.end);
+    }
+
+    properties.change = function(cb){
+
+        $(selector).on('apply.daterangepicker', function(ev, picker) {
+            var start = picker.startDate.format('YYYY-MM-DDT00:00'),
+                end = picker.endDate.format('YYYY-MM-DDT23:59'),
+                clear_date = undefined;
+            cb(start, end, clear_date);
+        });
+        $(selector).on('cancel.daterangepicker', function(ev, picker) {
+            var start = undefined,
+                end = undefined,
+                clear_date = 'clear';
+            cb(start, end, clear_date);
+        });
+
+    };
+
+    return properties
+}
+
