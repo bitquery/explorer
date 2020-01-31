f2d7150fa243ed873b374d8c72b825eaf4aceb1f
Andrey <bigzero@yandex.ru>
2020-01-31 16:59:09 +0300

    поиск

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def show
     if request.post?
-      redirect_to search_path(params[:query])
+      redirect_to search_path(params[:query], network: params[:network])
     end
     @query = params[:query]
   end
