eec76ded77839221a11be9fd473424bfbfa39a21
astudnev <astudnev@gmail.com>
2020-02-07 19:56:54 +0300

    config.public_file_server.enabled = true to avoid nginx installation for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,8 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.public_file_server.enabled = true
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
