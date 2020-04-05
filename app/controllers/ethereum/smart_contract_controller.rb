9956e54f86ad828888fe302892c12be498a42f54
astudnev <astudnev@gmail.com>
2020-04-05 06:31:11 +0300

    icons

--- app/controllers/ethereum/smart_contract_controller.rb
+++ app/controllers/ethereum/smart_contract_controller.rb
@@ -1,4 +1,17 @@
 class Ethereum::SmartContractController < NetworkController
   layout 'tabs'
 
+
+  def inflow
+    render 'ethereum/address/inflow'
+  end
+
+  def outflow
+    render 'ethereum/address/outflow'
+  end
+
+  def calls_contracts
+    render 'ethereum/address/calls_contracts'
+  end
+
 end
