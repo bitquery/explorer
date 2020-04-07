bac23e253d553937c5b853e6d3e363e37f957245
astudnev <astudnev@gmail.com>
2020-04-07 14:42:51 +0300

    call list

--- app/controllers/ethereum/tx_list_controller.rb
+++ app/controllers/ethereum/tx_list_controller.rb
@@ -1,15 +1,20 @@
 class Ethereum::TxListController < NetworkController
 
   def calls
-
+    @contract = params[:contract]
+    @caller = params[:caller]
+    @method = params[:method]
   end
 
   def transfers
-
+    @sender = params[:sender]
+    @receiver = params[:receiver]
+    @currency = params[:currency]
   end
 
   def events
-
+    @contract = params[:contract]
+    @method = params[:method]
   end
 
 end
