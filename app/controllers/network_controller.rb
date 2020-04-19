ea2b5809e7fa1cb21352b2e5d50a94683b55e8c6
astudnev <astudnev@gmail.com>
2020-04-19 20:39:02 +0300

    search path changed

--- app/controllers/network_controller.rb
+++ app/controllers/network_controller.rb
@@ -7,7 +7,7 @@ class NetworkController < ApplicationController
   def network_params
     raise "Network not defined" unless params[:network]
     @network = params[:network].kind_of?(ActionController::Parameters) ?
-                   params[:network].permit(:network, :tag, :name, :path, :family, :currency, :icon).to_h :
+                   params[:network].permit(:network, :tag, :name, :family, :currency, :icon).to_h :
                    BLOCKCHAIN_BY_NAME[params[:network]]
 
     if params[:address]
