e73f9d52f0f81d375fcd755e70381d37aa817c1a
astudnev <astudnev@gmail.com>
2020-05-06 14:42:33 +0300

    Bitcoin networks Block detailed pages

--- app/controllers/bitcoin/block_controller.rb
+++ app/controllers/bitcoin/block_controller.rb
@@ -1,3 +1,19 @@
 class Bitcoin::BlockController < NetworkController
   layout 'tabs'
+
+  before_action :query_date
+
+  QUERY =  BitqueryGraphql::Client.parse  <<-'GRAPHQL'
+           query ($height: Int! $network: BitcoinNetwork!){
+              bitcoin(network: $network ) { blocks( height: {is: $height}) { date {date} } }
+           }
+  GRAPHQL
+
+  private
+
+  def query_date
+    @block_date = BitqueryGraphql::Client.query(QUERY, variables: {height: @height.to_i,
+                                                                   network: @network[:network]}).data.bitcoin.blocks[0].date.date
+  end
+
 end
