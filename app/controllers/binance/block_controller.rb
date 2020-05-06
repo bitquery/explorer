a899809412f7bd6fc923d0ec7e389f82b19cc54b
astudnev <astudnev@gmail.com>
2020-05-06 14:30:11 +0300

    Binance network Block detailed pages

--- app/controllers/binance/block_controller.rb
+++ app/controllers/binance/block_controller.rb
@@ -1,3 +1,19 @@
 class Binance::BlockController < NetworkController
   layout 'tabs'
+
+  before_action :query_date
+
+  QUERY =  BitqueryGraphql::Client.parse  <<-'GRAPHQL'
+           query ($height: Int!){
+              binance { blocks( height: {is: $height}) { date {date} } }
+           }
+  GRAPHQL
+
+
+  private
+
+  def query_date
+    @block_date = BitqueryGraphql::Client.query(QUERY, variables: {height: @height.to_i}).data.binance.blocks[0].date.date
+  end
+
 end
