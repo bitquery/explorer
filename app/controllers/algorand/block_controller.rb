c4ac0dd6e6154653774cf94b43fa22577c308ef4
astudnev <astudnev@gmail.com>
2020-05-07 15:08:26 +0300

    algorand network controller

---
+++ app/controllers/algorand/block_controller.rb
@@ -0,0 +1,20 @@
+class Algorand::BlockController < NetworkController
+  layout 'tabs'
+
+  before_action :query_date
+
+  QUERY =  BitqueryGraphql::Client.parse  <<-'GRAPHQL'
+           query ($height: Int! $network: AlgorandNetwork!){
+              algorand(network: $network ) { blocks( height: {is: $height}) { date {date} } }
+           }
+  GRAPHQL
+
+
+  private
+
+  def query_date
+    @block_date = BitqueryGraphql::Client.query(QUERY, variables: {height: @height.to_i,
+                                                                   network: @network[:network]}).data.algorand.blocks[0].date.date
+  end
+
+end
