f615defaaf616a1ddda5a3bb25085112724b0d38
astudnev <astudnev@gmail.com>
2020-05-07 10:24:58 +0300

    icons for unknown networks

--- app/helpers/network_helper.rb
+++ app/helpers/network_helper.rb
@@ -1,8 +1,11 @@
 module NetworkHelper
 
-  def network_icon network
-    path = network[:icon].starts_with?('currency') ? network[:icon] : image_pack_path("media/icon/#{network[:icon]}")
-    image_tag path, { alt: network[:name], width: '32px', height: '32px', background_color: 'white'}
+  def network_icon_path network
+    network[:icon].starts_with?('currency') ? asset_path(network[:icon]) : image_pack_path("media/icon/#{network[:icon]}")
+  end
+
+  def network_icon network, options = {}
+    image_tag network_icon_path(network), { alt: network[:name] }.merge(options)
   end
 
   def dataset_icon dataset, size = 30
