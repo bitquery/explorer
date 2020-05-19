4bf18ac7d67a2ff7f085eefea24e594e42a5073c
astudnev <astudnev@gmail.com>
2020-05-19 14:29:50 +0300

    currency list for graph

--- app/controllers/ethereum/address_controller.rb
+++ app/controllers/ethereum/address_controller.rb
@@ -25,12 +25,45 @@ class Ethereum::AddressController < NetworkController
             }
   GRAPHQL
 
+  QUERY_CURRENCIES = BitqueryGraphql::Client.parse  <<-'GRAPHQL'
+   query($network: EthereumNetwork!, $address: String!) {
+              ethereum(network: $network) {
+                address(address: {is: $address}){
+                  address 
+                  annotation
+                  
+                  smartContract {
+                    contractType
+                    currency{
+                      symbol
+                      name
+                      decimals
+                      tokenType
+                    }
+                  }
+                  balance
+                }
+    						transfers(receiver: {is: $address}, options: {desc: "count"}){
+      							currency {
+                      address
+                      symbol
+                      name
+                    }
+      							count
+    						}
+              }
+            }
+  GRAPHQL
+
   private
 
   def query_graphql
     @address = params[:address]
-    @info = @address.starts_with?('0x') &&
-                BitqueryGraphql::Client.query(QUERY, variables: {network: @network[:network], address: @address}).data.ethereum.address.first
+    query = action_name == 'graph' ? QUERY_CURRENCIES : QUERY
+    result = @address.starts_with?('0x') &&
+        BitqueryGraphql::Client.query(query, variables: {network: @network[:network], address: @address}).data.ethereum
+    @info = result.address.first
+    @currencies = result.transfers.map(&:currency).sort_by{|c| c.address=='-' ? 0 : 1 } if result.try(:transfers)
   end
 
   def redirect_by_type
