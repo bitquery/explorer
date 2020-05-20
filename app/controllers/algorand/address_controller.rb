b0c010555936251a70bb0d18486a7b4e060fdc70
Andrey <bigzero@yandex.ru>
2020-05-20 18:37:11 +0300

    algorand graph

--- app/controllers/algorand/address_controller.rb
+++ app/controllers/algorand/address_controller.rb
@@ -1,3 +1,28 @@
 class Algorand::AddressController  < NetworkController
   layout 'tabs'
+  before_action :query_graphql
+
+  QUERY_CURRENCIES = BitqueryGraphql::Client.parse  <<-'GRAPHQL'
+query (  $address: String!){
+                        algorand{
+                          transfers(receiver: {is: $address}){
+                            currency{
+                              symbol
+                              tokenId
+                              name
+                            }
+                          }                    
+                        }
+                      }
+  GRAPHQL
+
+  private
+  def query_graphql
+    @address = params[:address]
+    if action_name == 'graph'
+      result = BitqueryGraphql::Client.query(QUERY_CURRENCIES, variables: {address: @address}).data.algorand
+      @currencies = result.transfers.map(&:currency).sort_by{|c| c.token_id == '0' ? 0 : 1 } if result.try(:transfers)
+    end
+  end
+
 end
