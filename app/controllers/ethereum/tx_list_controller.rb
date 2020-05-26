516ab4d567e504efd172c139532b35872e1eddea
Andrey <bigzero@yandex.ru>
2020-05-26 19:45:07 +0300

    tx_list breadcrumbs

--- app/controllers/ethereum/tx_list_controller.rb
+++ app/controllers/ethereum/tx_list_controller.rb
@@ -1,18 +1,21 @@
 class Ethereum::TxListController < NetworkController
 
   def calls
+    @breadcrumbs << {name: t('pages.tx_list.calls.breadcrumb')}
     @contract = params[:contract]
     @caller = params[:caller]
     @method = params[:method]
   end
 
   def transfers
+    @breadcrumbs << {name: t('pages.tx_list.transfers.breadcrumb')}
     @sender = params[:sender]
     @receiver = params[:receiver]
     @currency = params[:currency]
   end
 
   def events
+    @breadcrumbs << {name: t('pages.tx_list.events.breadcrumb')}
     @contract = params[:contract]
     @event = params[:event]
   end
