40048edc00df3582c666d9da074e937e3764eb1a
Andrey <bigzero@yandex.ru>
2020-06-29 17:57:14 +0300

    algorand graph to coinpath and add graph to binance

--- app/controllers/binance/address_controller.rb
+++ app/controllers/binance/address_controller.rb
@@ -1,3 +1,28 @@
 class Binance::AddressController < NetworkController
   layout 'tabs'
+
+  before_action :query_graphql
+
+  QUERY_CURRENCIES = BitqueryGraphql::Client.parse  <<-'GRAPHQL'
+query (  $address: String!){
+                        binance{
+                          transfers(receiver: {is: $address}){
+                            currency{
+                              symbol
+                              tokenId
+                              name
+                            }
+                          }                    
+                        }
+                      }
+  GRAPHQL
+
+  private
+  def query_graphql
+    @address = params[:address]
+    if action_name == 'graph'
+      result = BitqueryGraphql::Client.query(QUERY_CURRENCIES, variables: {address: @address}).data.binance
+      @currencies = result.transfers.map(&:currency).sort_by{|c| c.token_id == 'BNB' ? 0 : 1 } if result.try(:transfers)
+    end
+  end
 end
