4d0d5db7dc5831cb351de91115efeaf5daa0f584
Andrey <bigzero@yandex.ru>
2020-10-08 15:50:00 +0300

    v1.3.6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,7 +111,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  BITQUERY_WIDGETS_URL = "https://cdn.jsdelivr.net/gh/bitquery/widgets@v1.3.5"
+  BITQUERY_WIDGETS_URL = "https://cdn.jsdelivr.net/gh/bitquery/widgets@v1.3.6"
   BITQUERY_PROJECT_URL = "https://bitquery.io"
   BITQUERY_IMAGES = "#{BITQUERY_PROJECT_URL}/wp-content/uploads/2020/09"
   BITQUERY_GRAPHQL = 'https://graphql.bitquery.io'
