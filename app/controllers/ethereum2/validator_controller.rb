e8e64a9613b3d6276aeac17ee6141771a79a4257
astudnev <astudnev@gmail.com>
2020-10-19 21:15:38 +0300

    validator

--- app/controllers/ethereum2/validator_controller.rb
+++ app/controllers/ethereum2/validator_controller.rb
@@ -8,4 +8,8 @@ class Ethereum2::ValidatorController < NetworkController
   def get_index
     @index = params[:index]
   end
+
+  def breadcrumb
+    @breadcrumbs << {name: t("tabs.#{controller_name}.#{action_name}.name")}
+  end
 end
