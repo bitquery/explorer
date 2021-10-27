602f10e6391b9d39a4dd5830f784ad44afb804c4
Ruslan <kros8925@gmail.com>
2021-10-27 17:50:02 +0300

    Fixed money flow.

--- app/controllers/elrond/address_controller.rb
+++ app/controllers/elrond/address_controller.rb
@@ -10,11 +10,22 @@ module Elrond
     QUERY = BitqueryGraphql::Client.parse <<-'GRAPHQL'
       query ($network: ElrondNetwork!, $address: String!) {
         elrond(network: $network) {
-          transfers(
+          outflow: transfers(
             options: {desc: "count", limit: 100}
             transferSender: {is: $address}
           ) {
-            currency{
+            currency {
+              address
+              symbol
+              name
+            }
+            count
+          }
+          inflow: transfers(
+            options: {desc: "count", limit: 100}
+            transferReceiver: {is: $address}
+          ) {
+            currency {
               address
               symbol
               name
@@ -38,14 +49,15 @@ module Elrond
     end
 
     def query_graphql
-      begin
-        result = BitqueryGraphql::Client.query(QUERY, variables: { network: @network[:network], address: @address }).data.elrond
-        @currencies = result.transfers.map(&:currency).sort_by { |c| c.address == '-' ? 0 : 1 }.uniq { |x| x.address } if result.try(:transfers)
-      rescue Net::ReadTimeout => e
-        Raven.capture_exception e
-        sleep(1)
-        retry
-      end
+      result = BitqueryGraphql::Client.query(QUERY,
+                                             variables: { network: @network[:network],
+                                                          address: @address }).data.elrond
+      all_currencies = result.outflow + result.inflow
+      @currencies = all_currencies.map(&:currency).sort_by { |c| c.address == '-' ? 0 : 1 }.uniq { |x| x.address }
+    rescue Net::ReadTimeout => e
+      Raven.capture_exception e
+      sleep(1)
+      retry
     end
   end
 end
