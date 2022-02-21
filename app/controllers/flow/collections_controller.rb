8e80d34fa5cb382d83e7ac33502898a5e69080a0
Ruslan <kros8925@gmail.com>
2022-02-21 12:05:10 +0000

    Add collections.

---
+++ app/controllers/flow/collections_controller.rb
@@ -0,0 +1,21 @@
+module Flow
+  class CollectionsController < NetworkController
+    layout 'tabs'
+
+    before_action :set_collection_id
+
+    def show; end
+
+    def transactions; end
+
+    private
+
+    def set_collection_id
+      @id = params[:hash]
+    end
+
+    def breadcrumb
+      return if action_name == 'show'
+    end
+  end
+end
