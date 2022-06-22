5d15711026ce80c7f222fe7a7dbbbd583e55c036
Andrey Stepanov <bigzero@yandex.ru>
2022-06-22 15:56:13 +0300

    BIT-1320

--- app/controllers/network_controller.rb
+++ app/controllers/network_controller.rb
@@ -7,14 +7,14 @@ class NetworkController < ApplicationController
     @breadcrumbs = [
         {name: 'Blockchains', url: locale_path_prefix},
         {name: @network[:name], url: "#{locale_path_prefix}#{@network[:network]}"},
-        (params[:address] ? {name: "#{t("tabs.#{controller_name}.show.name")}: #{params[:address].truncate(15)}", url: "#{locale_path_prefix}#{@network[:network]}/#{params[:address]}"} : nil),
-        (params[:token1] ? {name: params[:token1].truncate(15), url: "#{locale_path_prefix}#{@network[:network]}/token/#{params[:token1]}"} : nil),
-        (params[:token2] ? {name: params[:token2].truncate(15), url: "#{locale_path_prefix}#{@network[:network]}/token/#{params[:token2]}"} : nil),
-        (params[:block] ? {name: "#{t("tabs.#{controller_name}.show.name")}: #{params[:block].truncate(15)}", url: "#{locale_path_prefix}#{@network[:network]}/#{params[:block]}"} : nil),
-        (params[:hash] ? {name: "#{t("tabs.#{controller_name}.show.name")}: #{params[:hash].truncate(15)}", url: "#{locale_path_prefix}#{@network[:network]}/#{params[:hash]}"} : nil),
-        ((params[:address]|| params[:block] || params[:hash]) && action_name != 'show' ? {name: t("tabs.#{controller_name}.#{action_name}.name"), url: "#{locale_path_prefix}#{@network[:network]}/#{params[:hash]}"} : nil)
+        (params[:address] ? {name: "#{t("tabs.#{controller_name}.show.name")}: #{params[:address].truncate(15)}", url: "#{locale_path_prefix}#{@network[:network]}/address/#{params[:address]}"} : nil),
+        (params[:token1] ? {name: "#{params[:token1].truncate(15)} - #{params[:token2].truncate(15)}", url: "#{locale_path_prefix}#{@network[:network]}/tokenpair/#{params[:token1]}/#{params[:token2]}"} : nil),
+        (params[:block] ? {name: "#{t("tabs.#{controller_name}.show.name")}: #{params[:block].truncate(15)}", url: "#{locale_path_prefix}#{@network[:network]}/block/#{params[:block]}"} : nil),
+        (params[:hash] ? {name: "#{t("tabs.#{controller_name}.show.name")}: #{params[:hash].truncate(15)}", url: "#{locale_path_prefix}#{@network[:network]}/tx/#{params[:hash]}"} : nil),
+        ((params[:address] || params[:block] || params[:hash]) && action_name != 'show' ? {name: t("tabs.#{controller_name}.#{action_name}.name"), url: "#{locale_path_prefix}#{@network[:network]}/#{params[:hash]}"} : nil)
     ].compact
   end
+
 
   def locale_path_prefix
     if params[:locale]
