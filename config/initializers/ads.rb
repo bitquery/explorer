70380ff97d33815184796d772d2e195735e62d6e
Andrey Stepanov <bigzero@yandex.ru>
2022-09-03 09:44:29 +0300

    BIT-2532

--- config/initializers/ads.rb
+++ config/initializers/ads.rb
@@ -36,17 +36,17 @@ ADS={
     # }
   },
   fixed: {
-    ad: {
-      title: 'Koinly: The #1 Crypto Tax Tool',
-      text: '',
-      button_text: 'Get Started For Free',
-      button_class: 'btn-primary',
-      # button_bg: '#335EEA',
-      # button_color: '#fff',
-      image: 'koinly_400x400.jpg',
-      url: 'https://koinly.io/au?via=BITQUERY&utm_source=paid-media-au&utm_medium=affiliates&utm_campaign=blockchain-explorer&utm_content=ad&utm_term=BITQUERY',
-      width: '25rem'
-    },
+    # ad: {
+    #   title: 'Koinly: The #1 Crypto Tax Tool',
+    #   text: '',
+    #   button_text: 'Get Started For Free',
+    #   button_class: 'btn-primary',
+    #   # button_bg: '#335EEA',
+    #   # button_color: '#fff',
+    #   image: 'koinly_400x400.jpg',
+    #   url: 'https://koinly.io/au?via=BITQUERY&utm_source=paid-media-au&utm_medium=affiliates&utm_campaign=blockchain-explorer&utm_content=ad&utm_term=BITQUERY',
+    #   width: '25rem'
+    # },
     # platform: {
     #   smart_contract: {
     #     ad: {
