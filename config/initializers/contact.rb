59e680bb0f03df9e6f732728b9cb6e662be0f606
crox <croxsafe@tutanota.com>
2022-11-15 21:42:13 -0300

    BIT-2823

---
+++ config/initializers/contact.rb
@@ -0,0 +1,12 @@
+CONTACTS = [
+    {
+        name: 'Discord',
+        url: 'https://discord.gg/38WKQqPmq7',
+        image: 'assets/discord-logo.svg'
+    },
+    {
+        name: 'Create a Ticket',
+        url: 'https://support.bitquery.io/',
+        image: 'assets/support-logo.svg'
+    }
+].freeze
