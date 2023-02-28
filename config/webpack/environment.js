062e53119199cdb035cd75ae90b361d6bcf7d01a
Spendil <spendil@mail.ru>
2023-02-28 13:50:26 +0300

    8

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,25 +1,5 @@
 const { environment,  } = require('@rails/webpacker')
-const EsmWebpackPlugin = require("@purtuga/esm-webpack-plugin");
 
-// const customConfig = {
-//     plugins: [ new EsmWebpackPlugin() ]
-// }
-
-// module.exports = merge(webpackConfig, customConfig)
-/* Fix a bug for file inclusion like <img :src="require()"/> */
-environment.loaders.get('file').use.find(item => item.loader === 'file-loader').options.esModule = false
-environment.config.merge({
-    /* module: {
-        rules: [
-          {
-            test: /\.mjs$/,
-            include: /node_modules/,
-            type: "javascript/auto"
-          }
-        ]
-      }, */
-    plugins: [ new EsmWebpackPlugin() ]
-  })
 module.exports = environment
 
 const webpack = require('webpack')
