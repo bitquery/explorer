191bc1e925ccc283c18985cd6b78a7865ea4031e
Nadia <nadia.olerinskaia@gmail.com>
2023-05-03 16:09:48 +0300

    accordion without style

--- app/javascript/packs/component/rendering/renderAccordion.js
+++ app/javascript/packs/component/rendering/renderAccordion.js
@@ -20,86 +20,52 @@ export default async function renderAccordion(uri) {
  
      return [
        {
-         title: 'Description',
+         header: 'Description',
          content: mediaDescription,
        },
        {
-         title: 'Data',
+         header: 'Data',
          content: JSON.stringify(mediaData, null, 2),
        },
      ];
    }
    console.log('mediaData',mediaData)
    const accordion = document.createElement('div');
+   accordion.style.zIndex = '100000'
    accordion.classList.add('accordion');
    accordion.id = 'accordionExample';
-   mediaData.forEach((e, index) =>{
+  
+      mediaData.forEach(item => {
+       const accordionItem = document.createElement('div');
+       accordionItem.classList.add('accordion-item');
 
-   const itemElement1 = document.createElement('div');
-   itemElement1.classList.add('accordion-item');
-   const headerElement1 = document.createElement('h5');
-   headerElement1.classList.add('accordion-header');
-   headerElement1.id = 'headingOne';
-   const buttonTitle1 = document.createElement('button');
-   buttonTitle1.classList.add('accordion-button');
-   buttonTitle1.setAttribute('type', 'button');
-   buttonTitle1.setAttribute('data-bs-toggle', 'collapse');
-   buttonTitle1.setAttribute('data-bs-target', '#collapseOne');
-   buttonTitle1.setAttribute('aria-expanded', 'false');
-   buttonTitle1.setAttribute('aria-controls', 'collapseOne');
-   buttonTitle1.textContent = e.title;
-   const collapseOne = document.createElement('div');
-   collapseOne.id='collapseOne';
-   collapseOne.classList.add('accordion-collapse','collapse')
-   collapseOne.setAttribute('aria-labelledby','headingOne')
-   collapseOne.setAttribute('data-bs-parent','#accordionExample')
-   const collapseOneBody = document.createElement('div');
-   collapseOneBody.classList.add('accordion-body');
-   collapseOneBody.textContent = e.content
+       const accordionHeader = document.createElement('div');
+       accordionHeader.classList.add('accordion-header');
+       accordionHeader.style.cursor = 'pointer'
+       accordionHeader.textContent = item.header;
+       accordionItem.appendChild(accordionHeader);
 
-   headerElement1.appendChild(buttonTitle1);
-   collapseOne.appendChild(collapseOneBody)
-   headerElement1.appendChild(collapseOne);
-   itemElement1.appendChild(headerElement1);
-   accordion.appendChild(itemElement1)
-})
+       const accordionContent = document.createElement('div');
+       accordionContent.classList.add('accordion-content');
+       accordionContent.innerHTML = item.content;
+       accordionItem.appendChild(accordionContent);
+       accordionContent.style.display = 'none';
 
-   // mediaData.forEach((item, index) => {
-   //   const itemId = `${accordionId}-item-${index}`;
- 
-   //   const itemElement = document.createElement('div');
-   //   itemElement.classList.add('accordion-item');
- 
-   //   const headerElement = document.createElement('h2');
-   //   headerElement.classList.add('accordion-header');
-   //   headerElement.id = `${itemId}-header`;
- 
-   //   const buttonElement = document.createElement('button');
-   //   buttonElement.classList.add('accordion-button', 'collapsed');
-   //   buttonElement.setAttribute('type', 'button');
-   //   buttonElement.setAttribute('data-bs-toggle', 'collapse');
-   //   buttonElement.setAttribute('data-bs-target', `#${itemId}`);
-   //   buttonElement.setAttribute('aria-expanded', 'false');
-   //   buttonElement.setAttribute('aria-controls', itemId);
-   //   buttonElement.textContent = item.title;
- 
-   //   const collapseElement = document.createElement('div');
-   //   collapseElement.classList.add('accordion-collapse', 'collapse');
-   //   collapseElement.id = itemId;
-   //   collapseElement.setAttribute('aria-labelledby', `${itemId}-header`);
-   //   collapseElement.setAttribute('data-bs-parent', `#${accordionId}`);
- 
-   //   const bodyElement = document.createElement('div');
-   //   bodyElement.classList.add('accordion-body');
-   //   bodyElement.innerHTML = item.content;
- 
-   //   headerElement.appendChild(buttonElement);
-   //   collapseElement.appendChild(bodyElement);
-   //   itemElement.appendChild(headerElement);
-   //   itemElement.appendChild(collapseElement);
-   //   accordion.appendChild(itemElement);
-   // });
- 
+       accordion.appendChild(accordionItem);
+   });
+
+   accordion.addEventListener('click', (event) => {
+       if (event.target.classList.contains('accordion-header')) {
+           const accordionContent = event.target.nextElementSibling;
+           if (accordionContent.style.display === 'block') {
+               accordionContent.style.display = 'none';
+           } else {
+               accordionContent.style.display = 'block';
+           }
+       }
+   });
+
+
    return accordion;
  }
  
