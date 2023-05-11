ce7dcf4c0a71956315c26b2184cba4d8736ad7ce
Spendil <spendil@mail.ru>
2023-05-11 15:24:37 +0700

    evnironment webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,13 @@
-const { environment,  } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker')
 
-module.exports = environment
+environment.config.optimization.minimizer[0].options.exclude = /render[A-Z].*.js/
+environment.config.optimization.minimizer[0].options.terserOptions.keep_classnames = true
+environment.config.optimization.minimizer[0].options.terserOptions.keep_fnames = true
+environment.config.optimization.minimizer[0].options.terserOptions.module = true
+const babelLoader = environment.loaders.get('babel')
+babelLoader.use[0].options.presets = [
+    ['@babel/preset-env', { targets: "defaults" }]
+]
 
 const webpack = require('webpack')
 environment.plugins.append(
@@ -11,3 +18,5 @@ environment.plugins.append(
         Popper: ['popper.js', 'default']
     })
 )
+
+module.exports = environment
