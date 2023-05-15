8f6af293e4b1fd9e5d1e82a2bd76710632973426
crox <croxsafe@tutanota.com>
2023-05-15 16:44:44 -0400

    BIT-4090

--- config/initializers/ads.rb
+++ config/initializers/ads.rb
@@ -2,11 +2,6 @@ ADS={
   tab: {
     ads: [
       {
-        text: 'Get Tax Report',
-        url: 'https://koinly.io/offer-bitquery/?utm_source=bitquery&utm_medium=cpm&utm_campaign=direct_buy&utm_term=apr_23',
-        bgcolor: 'green'
-      },
-      {
         text: 'Free DEX Swaps',
         url: 'https://jn3rg.app.link/Bitquery_1inch',
         bgcolor: '#0570f2'
@@ -14,7 +9,7 @@ ADS={
       {
         text: 'Verse DEX',
         url: 'https://verse.bitcoin.com/?utm_source=bitquery&utm_medium=web&utm_campaign=bitquery_explorer',
-        bgcolor: '#0085FF'
+        bgcolor: 'green'
       }
     ],
     # platform: {
@@ -40,7 +35,7 @@ ADS={
     ad: {
       title: 'Swap at the best rates!',
       title_color: '#000000',
-      text: 'No gas fees and MEV protection on 1inch - #1 DEX aggregator. Swap now!',
+      text: 'Gasless execution and MEV protection on 1inch - #1 DEX aggregator.',
       text_color: '#000000',
       button_text: 'Swap now!',
       button_class: 'btn-primary',
