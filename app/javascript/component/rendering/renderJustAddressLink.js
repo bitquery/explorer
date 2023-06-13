1d616da8abb1a6d4b6484fa1111592e06882bef9
Spendil <spendil@mail.ru>
2023-06-13 16:52:34 +0700

    fix links in renderers

--- app/javascript/component/rendering/renderJustAddressLink.js
+++ app/javascript/component/rendering/renderJustAddressLink.js
@@ -1,10 +1,9 @@
-import WidgetConfig from '../componentDescription/WidgetConfig'
 export default function renderJustAddressLink(data, variables, chainId) {
   const div = document.createElement('div');
   div.classList.add('text-truncate');
   const link = document.createElement('a');
   link.setAttribute('target', '_blank');
-  link.href = `/${WidgetConfig.getNetwork(chainId)}/address/${data}`; // Change  URL
+  link.href = `${process.env.EXPLORER_URL}/${WidgetConfig.getNetwork(chainId)}/address/${data}`;
   link.textContent = data;
 
   div.appendChild(link);
--- app/javascript/component/rendering/renderTX.js
+++ app/javascript/component/rendering/renderTX.js
@@ -1,10 +1,8 @@
-import WidgetConfig from '../componentDescription/WidgetConfig'
 export default function renderTX(str,variables,chainId) {
 	const div = document.createElement('div');
 	div.classList.add('text-truncate')
 	const link = document.createElement('a');
-	// link.setAttribute('target', 'blank');
-	link.href = `/${WidgetConfig.getNetwork(chainId)}/tx/${str}`; // Change  URL
+	link.href = `${process.env.EXPLORER_URL}/${WidgetConfig.getNetwork(chainId)}/tx/${str}`;
 	link.textContent = str;
 	div.appendChild(link)
 	return div;
