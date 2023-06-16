7879c39a4817a8e98dbedb4416d3cd5c9759ee14
Nadia <nadia.olerinskaia@gmail.com>
2023-06-16 08:01:03 +0300

    broken page

--- app/javascript/component/componentDescription/TradingGraphsComponent.js
+++ app/javascript/component/componentDescription/TradingGraphsComponent.js
@@ -4,26 +4,105 @@ export default class TradingGraphsComponent {
     this.config = this.configuration();
     this.variables = variables;
     this.chart = null; 
-    console.log(this.container)
+    this.data = null;
+    this.lastBar = null; 
+    this.lastData = null; 
   }
-  
-  
   
   async onData(data, sub) {
-    console.log(data)
-  new TradingView.widget({
-  container: this.container,
-  locale: 'en',
-  library_path: '/assets/charting_library/',
-  datafeed: new Datafeeds.UDFCompatibleDatafeed("https://demo-feed-data.tradingview.com"),
-  // datafeed: new Datafeeds.UDFCompatibleDatafeed('http://localhost:3000/datafeed', 1000),
-  symbol: 'AAPL',
-  interval: '1D',
-  fullscreen: true,
-  debug: true
-  });
+    const self = this;
+    self.data = data; 
+    if (self.chart) {
+      self.chart.onResetCacheNeededCallback();
+    }
 
-  
+    console.log(data);
+    const configurationData = {
+      supports_marks: false,
+      supports_timescale_marks: false,
+      supports_time: true,
+      supported_resolutions: ["D", "2D", "3D", "W", "3W", "M", "6M"]
+    }
+    
+    let resolution = 'D';
+    const symbolName = this.data.symbol
+    new TradingView.widget({
+      container: self.container,
+      locale: 'en',
+      library_path: '/assets/charting_library/',
+      datafeed: {
+        onReady: (callback) => {  
+          console.log('[onReady]: Method call');
+          setTimeout(() => callback(configurationData));
+        },
+        resolveSymbol: (symbolName, onSymbolResolvedCallback, onResolveErrorCallback, extension) => {
+          console.log('[resolveSymbol]: Method call', symbolName);
+          const splitSymbol = symbolName.split(/[:/]/);
+          setTimeout(() => {
+            const symbolInfo = {
+              name: symbolName,
+              description: symbolName,
+              session: '24x7',
+              timezone: 'Etc/UTC',
+              exchange: splitSymbol[0],
+              listed_exchange: splitSymbol[0],
+              minmov: 1,
+              pricescale: 10000,
+              data_status: 'streaming',
+              supported_resolutions: ['1', '5', '15', '30', '60', '1D', '1W', '1M']
+            }
+            onSymbolResolvedCallback(symbolInfo);
+          }, 0);
+        },
+        getBars: async (symbolInfo, resolution, periodParams, onHistoryCallback, onErrorCallback, firstDataRequest) => {
+          console.log('[getBars]: Method call', symbolInfo);
+          const bars = this.getBitqueryData(self.data).sort((a, b) => a.time - b.time);
+          console.log('bars', bars);
+          if(bars.length > 0) {
+            onHistoryCallback(bars, { noData: false });
+          } else {
+            console.log('No data');
+          }
+        },
+        // subscribeBars: (symbolInfo, resolution, onRealtimeCallback, subscriberUID, onResetCacheNeededCallback) => {
+        //   console.log('[subscribeBars]: Method call with subscriberUID:', subscriberUID);
+        //   self.lastBar = setInterval(async () => {
+        //     const latestData = this.getBitqueryData(self.data);
+        //     console.log('latestData', latestData);
+        //     if (latestData && latestData.length > 0 && JSON.stringify(self.lastData) !== JSON.stringify(latestData)) {
+        //       self.lastData = latestData;
+        //       // onRealtimeCallback(latestData);
+        //     }
+        //   }, 120000); 
+        // },
+        // unsubscribeBars: (subscriberUID) => {
+        //   console.log('[unsubscribeBars]: Method call with subscriberUID:', subscriberUID);
+        //   if (self.lastBar !== null) {
+        //     clearInterval(self.lastBar);
+        //     self.lastBar = null;
+        //     self.lastData = null;
+        //   }
+        // },
+      },
+      symbol: self.config.symbol,
+      interval: '1D',
+      fullscreen: true,
+      debug: false
+    });
   }
-}
+  getBitqueryData(data) {
+//     data.EVM.DEXTradeByTokens.forEach(e => {
+//       console.log('bladskoe vremya',new Date(e.Block.Time).getTime(),
+// )
+//     });
+    return data.EVM.DEXTradeByTokens.map(item => ({
+      time: new Date(item.Block.Time).getTime(),
+      low: item.Trade.low,
+      high: item.Trade.high,
+      open: item.Trade.open,
+      close: item.Trade.close,
+      volume: parseFloat(item.volume)
+    }))
+  }
 
+}
