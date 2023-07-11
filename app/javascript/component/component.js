de23fef1c3efcd3b9e6c4597e2366818751227a4
Spendil <spendil@mail.ru>
2023-07-12 06:43:53 +0700

    step 3

--- app/javascript/component/component.js
+++ app/javascript/component/component.js
@@ -1,171 +1,8 @@
-import { isNotEmptyArray, isNotEmptyObject } from "./helper";
+import { runWidget, createWidgetFrame, getBaseClass } from "./helper";
 
-const mapQueryAndVariablesStore = new Map();
-
-const subscribeWidget = async (subscribeWith, widgetInstance, onerror) => {
-	const currentUrl = subscribeWith.endpoint_url.replace(/^http/, 'ws');
-	const client = createClient({ url: currentUrl });
-	const payload = { query: subscribeWith.query, variables: subscribeWith.variables };
-
-	client.subscribe(payload, {
-		next: ({ data }) => {
-			const sub = 'subscription';
-			widgetInstance.onData(data, sub);
-		},
-		error: error => {
-			onerror(error);
-		},
-		complete: () => console.log('complete'),
-	});
-};
-
-const runQuery = async (payload, api_key = '') => {
-	const response = await fetch(payload.endpoint_url, {
-		method: 'POST',
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/json',
-			'X-API-KEY': api_key
-		},
-		body: JSON.stringify({ query: payload.query, variables: payload.variables }),
-		credentials: 'same-origin',
-	});
-	if (response.status !== 200) {
-		throw new Error(response.error);
-	}
-	const { data } = await response.json();
-	if (data.errors) {
-		throw new Error(data.errors[0].message);
-	}
-	return data
-};
-
-const renderQueryInComponent = async (payload, widgetInstance, api_key) => {
-	const data = await runQuery(payload, api_key);
-	widgetInstance.onData(data);
-};
-
-const prepopulateQuery = async (url, componentObject, compElement, query, queryVariables, prePopulateId, queryId, api_key) => {
-	const startQuery = query;
-	let finalQuery = startQuery;
-	if (prePopulateId) {
-		const response = await fetch(`${window.bitqueryAPI}/getquery/${prePopulateId}`);
-		let queryMetaData;
-		if (response.status === 200) {
-			queryMetaData = await response.json();
-		} else {
-			throw new Error(response.error);
-		}
-		finalQuery = queryMetaData.query.trim();
-		const queryVariables = {
-			...JSON.parse(queryMetaData.variables),
-			...queryVariables,
-		};
-	}
-	const payload = {
-		endpoint_url: url,
-		query: finalQuery,
-		variables: queryVariables
-	}
-	await renderQueryInComponent(payload, componentObject, api_key);
-};
-
-const createWidgetFrame = (componentClass, selector, queryId) => {
-	const componentContainer = document.querySelector(selector);
-	const widgetHeader = document.createElement('div');
-	const row = document.createElement('div');
-	const col8 = document.createElement('div');
-	const cardBody = document.createElement('div');
-	const widgetFrame = document.createElement('div');
-	const tableFooter = document.createElement('div');
-	const getAPIButton = document.createElement('a');
-	const showMoreButton = document.createElement('a');
-	const loader = document.createElement('div');
-	const blinkerWrapper = document.createElement('div');
-	loader.classList.add('lds-dual-ring');
-	getAPIButton.classList.add('badge', 'badge-secondary', 'open-btn', 'bg-success', 'get-api');
-	getAPIButton.setAttribute('role', 'button');
-	getAPIButton.setAttribute('target', '_blank');
-	getAPIButton.textContent = 'Get Streaming API';
-	showMoreButton.classList.add('more-link');
-	showMoreButton.textContent = 'Show more...';
-	showMoreButton.style.display = 'none';
-	showMoreButton.style.cursor = 'pointer';
-	tableFooter.style.display = 'flex';
-	tableFooter.style.justifyContent = 'end';
-	tableFooter.appendChild(showMoreButton);
-	tableFooter.appendChild(getAPIButton);
-	widgetHeader.classList.add('card-header');
-	row.classList.add('row');
-	col8.classList.add('col');
-	// col8.classList.add('col-8');
-	cardBody.classList.add('card-body', 'text-center');
-	widgetFrame.classList.add('widget-container', 'tabulator');
-	// widgetFrame.style.height = '470px';
-	widgetFrame.style.height = 'fit-content';
-	// widgetFrame.style.overflow = 'scroll';
-	componentContainer.appendChild(widgetHeader);
-	componentContainer.appendChild(cardBody);
-	cardBody.appendChild(widgetFrame);
-	cardBody.appendChild(tableFooter);
-	widgetHeader.appendChild(row);
-	row.appendChild(col8);
-	const onloadmetadata = queryMetaData => {
-		col8.textContent = queryMetaData?.name || 'No query presented';
-		if (queryMetaData.query.match(/subscription[^a-zA-z0-9]/gm)) {
-			const liveSpan = document.createElement('span');
-			const blinker = document.createElement('div');
-			blinkerWrapper.classList.add('col-4', 'text-success', 'text-right');
-			blinker.classList.add('blink', 'blnkr', 'bg-success');
-			row.appendChild(blinkerWrapper);
-			liveSpan.classList.add('d-none', 'd-sm-inline');
-			liveSpan.textContent = 'Live';
-			blinkerWrapper.appendChild(liveSpan);
-			blinkerWrapper.appendChild(blinker);
-		}
-	};
-	const onquerystarted = () => {
-		cardBody.appendChild(loader);
-	};
-	const onqueryend = () => {
-		cardBody.removeChild(loader);
-	};
-	const onerror = error => {
-		if (row.contains(blinkerWrapper)) {
-			row.removeChild(blinkerWrapper);
-		}
-		cardBody.textContent = '';
-		cardBody.classList.add('alert', 'alert-danger');
-		cardBody.setAttribute('role', 'alert');
-		const title = document.createElement('h4');
-		title.classList.add('alert-heading');
-		const errorText = document.createElement('div');
-		errorText.classList.add('mb-0');
-		cardBody.appendChild(title);
-		cardBody.appendChild(errorText);
-		title.textContent = 'An error occurred while executing the request. Please, try again later';
-		if (Array.isArray(error)) {
-			errorText.textContent = `Error: ${error[0].message}`;
-		} else if ('message' in error) {
-			errorText.textContent = `Error: ${error.message}`;
-		} else {
-			errorText.textContent = `Error: ${error}`;
-		}
-	};
-	return {
-		frame: widgetFrame,
-		button: getAPIButton,
-		button2: showMoreButton,
-		onloadmetadata,
-		onquerystarted,
-		onqueryend,
-		onerror,
-	};
-};
-
-export default async function renderComponent(component, selector, queryId, variables = {}, prePopulateId, getNewDataForQuery, api_key) {
+export default async function renderComponent(component, selector, queryId, variables = {}, prepopulateQueryID, getNewDataForQuery, api_key) {
 	document.querySelector(selector).textContent = '';
-	const widgetFrame = createWidgetFrame(component, selector, queryId);
+	const widgetFrame = createWidgetFrame(selector);
 	let queryMetaData;
 	try {
 		const response = await fetch(`${window.bitqueryAPI}/getquery/${queryId}`);
@@ -181,108 +18,12 @@ export default async function renderComponent(component, selector, queryId, vari
 		widgetFrame.onloadmetadata(queryMetaData);
 		const compElement = widgetFrame.frame;
 		const query = queryMetaData.query.trim();
-
-		async function getNewDataForQuery(interval = '', from, till, limit = 9990) {
-			widgetFrame.button2.style.display = 'none'
-
-			const addVariables = {
-				interval: interval,
-				till: till,
-				from: from,
-				limit: limit,
-			};
-			const queryVariables = {
-				...JSON.parse(queryMetaData.variables),
-				...variables,
-				...addVariables,
-			};
-			const payload = {
-				endpoint_url: queryMetaData.endpoint_url,
-				query,
-				variables: queryVariables
-			}
-
-			if (query.startsWith('subscription')) {
-				await prepopulateQuery(queryMetaData.endpoint_url, componentObject, compElement, query, queryVariables, prePopulateId, queryId, api_key);
-				subscribeWidget(payload, componentObject, widgetFrame.onerror);
-			} else {
-				await renderQueryInComponent(payload, componentObject, api_key);
-			}
-		}
-
-		async function getNewLimitForShowMoreButton() {
-			widgetFrame.onquerystarted();
-			queryVariables.limit += 10
-			console.log('queryVariables.limit', queryVariables.limit)
-			componentObject.clearData();
-			const payload = {
-				endpoint_url: queryMetaData.endpoint_url,
-				query,
-				variables: queryVariables
-			}
-			if (query.startsWith('subscription')) {
-				await prepopulateQuery(queryMetaData.endpoint_url, componentObject, compElement, query, queryVariables, prePopulateId, queryId, api_key);
-				subscribeWidget(payload, componentObject, widgetFrame.onerror);
-			} else {
-				await renderQueryInComponent(payload, componentObject, api_key);
-			}
-			widgetFrame.onqueryend();
-		}
-
 		const queryVariables = {
 			...JSON.parse(queryMetaData.variables),
 			...variables,
 		};
-		if (Object.getPrototypeOf(component) === BootstrapTableComponent || Object.getPrototypeOf(component) === BootstrapCardComponent) {
-			widgetFrame.button2.parentNode.style.justifyContent = 'space-between';
-			widgetFrame.button2.style.display = 'block'
-			if (query.startsWith('subscription')) {
-				widgetFrame.button2.style.display = 'none'
-				widgetFrame.button2.parentNode.style.justifyContent = 'end';
 
-			}
-		}
-		const componentObject = new component(compElement, queryVariables, getNewDataForQuery);
-		const data = [];
-		data.push({ [WidgetConfig.name]: serialize(WidgetConfig) });
-		function getBaseClass(targetClass) {
-			data.push({ [targetClass.name]: serialize(targetClass) });
-			if (targetClass instanceof Function) {
-				let baseClass = targetClass;
-				while (baseClass) {
-					const newBaseClass = Object.getPrototypeOf(baseClass);
-					if (newBaseClass && newBaseClass !== Object && newBaseClass.name) {
-						baseClass = newBaseClass;
-						data.unshift({ [baseClass.name]: serialize(baseClass) });
-					} else {
-						break;
-					}
-				}
-			}
-		}
-		function discoverFunctions(subj, prop) {
-			if (typeof subj === 'function') {
-				if (prop && subj?.name !== prop) {
-					if (!data.some(el => Object.keys(el)[0] === subj?.name)) {
-						data.unshift({ [subj.name]: serialize(subj) });
-					}
-				}
-				return;
-			}
-			if (isNotEmptyArray(subj)) {
-				for (let el of subj) {
-					discoverFunctions(el);
-				}
-			}
-			if (isNotEmptyObject(subj)) {
-				for (let prop in subj) {
-					discoverFunctions(subj[prop], prop);
-				}
-			}
-		}
-		getBaseClass(component);
-		discoverFunctions(componentObject.config);
-		widgetFrame.button.onclick = () => {
+		const getStreamingAPI = () => {
 			let createHiddenField = function (name, value) {
 				let input = document.createElement('input');
 				input.setAttribute('type', 'hidden');
@@ -302,22 +43,67 @@ export default async function renderComponent(component, selector, queryId, vari
 			form.submit();
 			document.body.removeChild(form);
 		};
-		widgetFrame.onquerystarted();
+		async function getNewDataForQuery(interval) {
+			widgetFrame.button2.style.display = 'none'
 
+			const addVariables = {
+				interval,
+				limit: 9990,
+			};
+			const queryVariables = {
+				...JSON.parse(queryMetaData.variables),
+				...variables,
+				...addVariables,
+			};
+			const payload = {
+				endpoint_url: queryMetaData.endpoint_url,
+				query,
+				variables: queryVariables,
+				prepopulateQueryID
+			}
+
+			runWidget(payload, componentObject, api_key, widgetFrame.onerror)
+		}
+		async function getNewLimitForShowMoreButton() {
+			widgetFrame.onquerystarted();
+			queryVariables.limit += 10
+			console.log('queryVariables.limit', queryVariables.limit)
+			componentObject.clearData();
+			const payload = {
+				endpoint_url: queryMetaData.endpoint_url,
+				query,
+				variables: queryVariables,
+				prepopulateQueryID
+			}
+			runWidget(payload, componentObject, api_key, widgetFrame.onerror)
+		}
+		
+		if (Object.getPrototypeOf(component) === BootstrapTableComponent || Object.getPrototypeOf(component) === BootstrapCardComponent) {
+			widgetFrame.button2.parentNode.style.justifyContent = 'space-between';
+			widgetFrame.button2.style.display = 'block'
+			if (query.startsWith('subscription')) {
+				widgetFrame.button2.style.display = 'none'
+				widgetFrame.button2.parentNode.style.justifyContent = 'end';
+
+			}
+		}
+		const componentObject = new component(compElement, queryVariables, getNewDataForQuery);
+
+		const data = getBaseClass(component, componentObject.config);
+		data.unshift({ [WidgetConfig.name]: serialize(WidgetConfig) });
+		
+		widgetFrame.button.onclick = getStreamingAPI
+		widgetFrame.onquerystarted();
 		widgetFrame.button2.onclick = () => getNewLimitForShowMoreButton()
 
 		const payload = {
 			endpoint_url: queryMetaData.endpoint_url,
 			query,
-			variables: queryVariables
+			variables: queryVariables,
+			prepopulateQueryID
 		}
 
-		if (query.startsWith('subscription')) {
-			await prepopulateQuery(queryMetaData.endpoint_url, componentObject, compElement, query, queryVariables, prePopulateId, queryId, api_key);
-			subscribeWidget(payload, componentObject, widgetFrame.onerror);
-		} else {
-			await renderQueryInComponent(payload, componentObject, api_key);
-		}
+		runWidget(payload, componentObject, api_key, widgetFrame.onerror)
 		widgetFrame.onqueryend();
 	} catch (error) {
 		widgetFrame.onerror(error);
--- app/javascript/component/helper.js
+++ app/javascript/component/helper.js
@@ -1,4 +1,206 @@
-export const isNotEmptyArray = subj => Array.isArray(subj) && subj.length;
+const isNotEmptyArray = subj => Array.isArray(subj) && subj.length;
+const isNotEmptyObject = subj => !Array.isArray(subj) && typeof subj === 'object' && Object.keys(subj).length;
 
-export const isNotEmptyObject = subj => !Array.isArray(subj) && typeof subj === 'object' && Object.keys(subj).length;
+export const getBaseClass = (targetClass, config) => {
+	const discoverFunctions = (subj, prop) => {
+		if (typeof subj === 'function') {
+			if (prop && subj?.name !== prop) {
+				if (!data.some(el => Object.keys(el)[0] === subj?.name)) {
+					data.unshift({ [subj.name]: serialize(subj) });
+				}
+			}
+			return;
+		}
+		if (isNotEmptyArray(subj)) {
+			for (let el of subj) {
+				discoverFunctions(el);
+			}
+		}
+		if (isNotEmptyObject(subj)) {
+			for (let prop in subj) {
+				discoverFunctions(subj[prop], prop);
+			}
+		}
+	}
+	const data = []
+	data.push({ [targetClass.name]: serialize(targetClass) });
+	if (targetClass instanceof Function) {
+		let baseClass = targetClass;
+		while (baseClass) {
+			const newBaseClass = Object.getPrototypeOf(baseClass);
+			if (newBaseClass && newBaseClass !== Object && newBaseClass.name) {
+				baseClass = newBaseClass;
+				data.unshift({ [baseClass.name]: serialize(baseClass) });
+			} else {
+				break;
+			}
+		}
+	}
+	discoverFunctions(config)
+	return data
+}
 
+export const runWidget = (payload, widgetInstance, api_key, onerror) => {
+
+	const runQuery = async (payload) => {
+		const response = await fetch(payload.endpoint_url, {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+				'X-API-KEY': api_key
+			},
+			body: JSON.stringify({ query: payload.query, variables: payload.variables }),
+			credentials: 'same-origin',
+		});
+		if (response.status !== 200) {
+			throw new Error(response.error);
+		}
+		const { data } = await response.json();
+		if (data.errors) {
+			throw new Error(data.errors[0].message);
+		}
+		return data
+	};
+
+	const renderQueryInComponent = async (payload) => {
+		const data = await runQuery(payload);
+		widgetInstance.onData(data);
+	};
+
+	const prepopulateWidget = async () => {
+		if (!payload.prepopulateQueryID) {
+			return
+		}
+		console.log('prepopulate')
+		let queryMetaData
+		const response = await fetch(`${window.bitqueryAPI}/getquery/${payload.prepopulateQueryID}`);
+		if (response.status === 200) {
+			queryMetaData = await response.json();
+		} else {
+			throw new Error(response.error);
+		}
+		const currentPayload = {
+			endpoint_url: payload.endpoint_url,
+			query: queryMetaData.query,
+			variables: {
+				...JSON.parse(queryMetaData.variables),
+				...payload.variables,
+			}
+		}
+		renderQueryInComponent(currentPayload);
+	};
+
+	const subscribeWidget = async () => {
+		const currentUrl = payload.endpoint_url.replace(/^http/, 'ws');
+		const client = createClient({ url: currentUrl });
+
+		client.subscribe({ query: payload.query, variables: payload.variables }, {
+			next: ({ data }) => {
+				const sub = 'subscription';
+				widgetInstance.onData(data, sub);
+			},
+			error: error => {
+				onerror(error);
+			},
+			complete: () => console.log('complete'),
+		});
+	};
+
+	if (payload.query.startsWith('subscription')) {
+		prepopulateWidget()
+		subscribeWidget()
+	} else {
+		renderQueryInComponent(payload)
+	}
+}
+
+export const createWidgetFrame = selector => {
+	const componentContainer = document.querySelector(selector);
+	const widgetHeader = document.createElement('div');
+	const row = document.createElement('div');
+	const col8 = document.createElement('div');
+	const cardBody = document.createElement('div');
+	const widgetFrame = document.createElement('div');
+	const tableFooter = document.createElement('div');
+	const getAPIButton = document.createElement('a');
+	const showMoreButton = document.createElement('a');
+	const loader = document.createElement('div');
+	const blinkerWrapper = document.createElement('div');
+	loader.classList.add('lds-dual-ring');
+	getAPIButton.classList.add('badge', 'badge-secondary', 'open-btn', 'bg-success', 'get-api');
+	getAPIButton.setAttribute('role', 'button');
+	getAPIButton.setAttribute('target', '_blank');
+	getAPIButton.textContent = 'Get Streaming API';
+	showMoreButton.classList.add('more-link');
+	showMoreButton.textContent = 'Show more...';
+	showMoreButton.style.display = 'none';
+	showMoreButton.style.cursor = 'pointer';
+	tableFooter.style.display = 'flex';
+	tableFooter.style.justifyContent = 'end';
+	tableFooter.appendChild(showMoreButton);
+	tableFooter.appendChild(getAPIButton);
+	widgetHeader.classList.add('card-header');
+	row.classList.add('row');
+	col8.classList.add('col');
+	cardBody.classList.add('card-body', 'text-center');
+	widgetFrame.classList.add('widget-container', 'tabulator');
+	widgetFrame.style.height = 'fit-content';
+	componentContainer.appendChild(widgetHeader);
+	componentContainer.appendChild(cardBody);
+	cardBody.appendChild(widgetFrame);
+	cardBody.appendChild(tableFooter);
+	widgetHeader.appendChild(row);
+	row.appendChild(col8);
+	const onloadmetadata = queryMetaData => {
+		col8.textContent = queryMetaData?.name || 'No query presented';
+		if (queryMetaData.query.match(/subscription[^a-zA-z0-9]/gm)) {
+			const liveSpan = document.createElement('span');
+			const blinker = document.createElement('div');
+			blinkerWrapper.classList.add('col-4', 'text-success', 'text-right');
+			blinker.classList.add('blink', 'blnkr', 'bg-success');
+			row.appendChild(blinkerWrapper);
+			liveSpan.classList.add('d-none', 'd-sm-inline');
+			liveSpan.textContent = 'Live';
+			blinkerWrapper.appendChild(liveSpan);
+			blinkerWrapper.appendChild(blinker);
+		}
+	};
+	const onquerystarted = () => {
+		cardBody.appendChild(loader);
+	};
+	const onqueryend = () => {
+		cardBody.removeChild(loader);
+	};
+	const onerror = error => {
+		if (row.contains(blinkerWrapper)) {
+			row.removeChild(blinkerWrapper);
+		}
+		cardBody.textContent = '';
+		cardBody.classList.add('alert', 'alert-danger');
+		cardBody.setAttribute('role', 'alert');
+		const title = document.createElement('h4');
+		title.classList.add('alert-heading');
+		const errorText = document.createElement('div');
+		errorText.classList.add('mb-0');
+		cardBody.appendChild(title);
+		cardBody.appendChild(errorText);
+		title.textContent = 'An error occurred while executing the request. Please, try again later';
+		if (Array.isArray(error)) {
+			errorText.textContent = `Error: ${error[0].message}`;
+		} else if ('message' in error) {
+			errorText.textContent = `Error: ${error.message}`;
+		} else {
+			errorText.textContent = `Error: ${error}`;
+		}
+	};
+	return {
+		frame: widgetFrame,
+		button: getAPIButton,
+		button2: showMoreButton,
+		onloadmetadata,
+		onquerystarted,
+		onqueryend,
+		onerror,
+	};
+}
