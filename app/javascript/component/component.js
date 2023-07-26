dab0e87337f5865726f3c82e90f1a27c84cd1bd8
Spendil <spendil@mail.ru>
2023-07-26 12:57:14 +0700

    dataSources implementation

--- app/javascript/component/component.js
+++ app/javascript/component/component.js
@@ -12,29 +12,75 @@ export default async function renderComponent(component, selector, queryID, expl
 	document.querySelector(selector).textContent = '';
 	const widgetFrame = createWidgetFrame(selector);
 	try {
-		const queryParams = await getQueryParams(queryID)
-		const { endpoint_url, query, variables: rawVariables } = queryParams
-		widgetFrame.onloadmetadata(queryParams);
+		const subscriptionQueryParams = await getQueryParams(queryID)
+		const { endpoint_url, query, variables: rawVariables } = subscriptionQueryParams
+		widgetFrame.onloadmetadata(subscriptionQueryParams);
 		const compElement = widgetFrame.frame;
 		const variables = { ...rawVariables, ...explorerVariables };
+		const subscriptionPayload = { query, variables, endpoint_url }
 
-		async function getNewLimitForShowMoreButton() {
-			widgetFrame.onquerystarted();
-			variables.limit += 10
-			componentObject.clearData();
-			const payload = { endpoint_url, query, variables, prepopulateQueryID }
-			runWidget(payload, componentObject, api_key, widgetFrame.onerror)
-		}
-		if (Object.getPrototypeOf(component) === BootstrapTableComponent || Object.getPrototypeOf(component) === BootstrapCardComponent) {
-			widgetFrame.button2.parentNode.style.justifyContent = 'space-between';
-			widgetFrame.button2.style.display = 'block'
-			if (query.startsWith('subscription')) {
-				widgetFrame.button2.style.display = 'none'
-				widgetFrame.button2.parentNode.style.justifyContent = 'end';
+		function SubscriptionDataSource(payload, onerror) {
+
+			const subscribe = () => {
+				const currentUrl = payload.endpoint_url.replace(/^http/, 'ws');
+				const client = createClient({ url: currentUrl });
+
+				this.cleanSubscription = client.subscribe({ query: payload.query, variables: payload.variables }, {
+					next: this.callback,
+					error: error => {
+						console.log(error)
+						onerror(error);
+					},
+					complete: () => console.log('complete'),
+				});
+			} 
+
+			const setCallback = callback => {
+				this.callback = callback
+				subscribe()
 			}
-		}
 
-		const componentObject = new component(compElement, {variables}, getData);
+			const changeVariables = async deltaVariables => {
+				this.variables = { ...payload.variables, ...deltaVariables }
+				this.cleanSubscription()
+				subscribe()
+			}
+
+			return {
+				setCallback,
+				changeVariables
+			}
+
+		}
+		const subscriptionDataSource = new SubscriptionDataSource( subscriptionPayload, widgetFrame.onerror )
+
+		const historyQueryParams = await getQueryParams(prepopulateQueryID)
+		const historyPayload = {
+			variables,
+			query: historyQueryParams.query,
+			endpoint_url: historyQueryParams.endpoint_url
+		}
+		function HistoryDataSource(payload) {
+
+			const setCallback = callback => {
+				this.callback = callback
+			}
+
+			const changeVariables = async deltaVariables => {
+				this.variables = { ...payload.variables, ...deltaVariables }
+				this.data = await getData({ ...payload, variables: this.variables })
+				this.callback(data)
+			}
+
+			return {
+				setCallback,
+				changeVariables
+			}
+
+		}
+		const historyDataSource = new HistoryDataSource(historyPayload)
+
+		const componentObject = new component(compElement, historyDataSource, subscriptionDataSource);
 
 		const data = getBaseClass(component, componentObject.config);
 		data.unshift({ [WidgetConfig.name]: serialize(WidgetConfig) });
@@ -44,7 +90,9 @@ export default async function renderComponent(component, selector, queryID, expl
 		widgetFrame.button2.onclick = getNewLimitForShowMoreButton
 		widgetFrame.onquerystarted();
 
-		const payload = { endpoint_url, query, variables, prepopulateQueryID }
+
+		
+		
 		if (componentObject.constructor.name === 'OHLCbyIntervalsGraph') {
 			componentObject.onData(undefined, true)
 		} else {
