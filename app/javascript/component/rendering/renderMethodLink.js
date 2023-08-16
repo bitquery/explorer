ba72c1d0a3d84ab9ee7943c2a66d7aa00eadeebd
Nadia <nadia.olerinskaia@gmail.com>
2023-08-16 16:19:45 +0300

    hide arbitrum

--- app/javascript/component/rendering/renderMethodLink.js
+++ app/javascript/component/rendering/renderMethodLink.js
@@ -4,8 +4,8 @@ export default function renderMethodLink(data, variables, chainId) {
 	const link = document.createElement('a');
     link.setAttribute('title',data.method)
 
-    // link.href = `https://explorer.bitquery.io/${WidgetConfig.getNetwork(chainId)}/method/${data.hash}`;
-    link.href = `/${WidgetConfig.getNetwork(chainId)}/method/${data.hash}`;
+    link.href = `https://explorer.bitquery.io/${WidgetConfig.getNetwork(chainId)}/method/${data.hash}`;
+    // link.href = `/${WidgetConfig.getNetwork(chainId)}/method/${data.hash}`;
     link.textContent = data.method || '-'
 
 	div.appendChild(link)
