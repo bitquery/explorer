a0da1896c2f1ec8a69d12f763c6217e1880ce1b0
Nadia <nadia.olerinskaia@gmail.com>
2023-09-07 13:37:56 +0300

    fix renderNumbers

--- app/javascript/component/rendering/renderNumbers.js
+++ app/javascript/component/rendering/renderNumbers.js
@@ -1,9 +1,30 @@
+// export default function renderNumbers(data) {
+//     const div = document.createElement('div');
+//     div.style.cssText = 'text-align: end; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;';
+//
+//     const span = document.createElement('span');
+//     span.textContent = parseFloat(+data);
+//
+//     div.setAttribute('title', data);
+//     div.appendChild(span);
+//
+//     return div;
+// }
 export default function renderNumbers(data) {
     const div = document.createElement('div');
     div.style.cssText = 'text-align: end; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;';
-
     const span = document.createElement('span');
-    span.textContent = parseFloat(+data);
+
+    if (/^-?\d+$/.test(data)) {
+        const bigIntValue = BigInt(data);
+        if (bigIntValue > Number.MAX_SAFE_INTEGER || bigIntValue < Number.MIN_SAFE_INTEGER) {
+            span.textContent = data;
+        } else {
+            span.textContent = parseFloat(+data);
+        }
+    } else {
+        span.textContent = data;
+    }
 
     div.setAttribute('title', data);
     div.appendChild(span);
