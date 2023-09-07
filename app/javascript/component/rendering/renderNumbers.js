44822e253cd5eb9e6ab0c784569c925776b4ce76
Nadia <nadia.olerinskaia@gmail.com>
2023-09-07 13:48:01 +0300

    fix addres->transaction page

--- app/javascript/component/rendering/renderNumbers.js
+++ app/javascript/component/rendering/renderNumbers.js
@@ -1,15 +1,3 @@
-// export default function renderNumbers(data) {
-//     const div = document.createElement('div');
-//     div.style.cssText = 'text-align: end; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;';
-//
-//     const span = document.createElement('span');
-//     span.textContent = parseFloat(+data);
-//
-//     div.setAttribute('title', data);
-//     div.appendChild(span);
-//
-//     return div;
-// }
 export default function renderNumbers(data) {
     const div = document.createElement('div');
     div.style.cssText = 'text-align: end; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;';
