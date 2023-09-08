6cd0ad2153da3d7848843097247b30aac2d5388b
Nadia <nadia.olerinskaia@gmail.com>
2023-09-08 11:04:22 +0300

    arbitrum ready

--- app/javascript/component/rendering/renderTokenLink.js
+++ app/javascript/component/rendering/renderTokenLink.js
@@ -11,8 +11,8 @@ export default function renderTokenLink(data, variables, chainId) {
 		link.setAttribute('title', data.smartContract)
 
 	}
-	// link.href = `https://explorer.bitquery.io/${WidgetConfig.getNetwork(chainId)}/token/${data.smartContract}`;
-	link.href = `/${WidgetConfig.getNetwork(chainId)}/token/${data.smartContract}`;
+	link.href = `https://explorer.bitquery.io/${WidgetConfig.getNetwork(chainId)}/token/${data.smartContract}`;
+	// link.href = `/${WidgetConfig.getNetwork(chainId)}/token/${data.smartContract}`;
 	div.appendChild(link);
 	return div;
 }
