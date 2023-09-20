c80f41ed594f06b7120bfde28f61ac0f62653856
Spendil <spendil@mail.ru>
2023-09-20 13:58:58 +0700

    update components

--- app/javascript/component/componentDescription/BaseComponent.js
+++ app/javascript/component/componentDescription/BaseComponent.js
@@ -9,11 +9,9 @@ export default class BaseComponent {
 	async init() {
 		if (this.historyDataSource) {
 			this.historyDataSource.setCallback(this.onHistoryData.bind(this))
-			this.historyDataSource && await this.historyDataSource.changeVariables()
 		}
 		if (this.subscriptionDataSource) {
 			this.subscriptionDataSource.setCallback(this.onSubscriptionData.bind(this))
-			this.subscriptionDataSource.changeVariables()
 		}
 	}
 }
