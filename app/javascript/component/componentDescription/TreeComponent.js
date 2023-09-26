67f97749008d1af9111e8172e0125bdc685d2e28
Nadia <nadia.olerinskaia@gmail.com>
2023-09-26 16:43:27 +0300

    fix trading page, added path

--- app/javascript/component/componentDescription/TreeComponent.js
+++ app/javascript/component/componentDescription/TreeComponent.js
@@ -85,7 +85,6 @@ export default class TreeComponent {
                     .sort((a, b) => a.Log.Index - b.Log.Index)
                     .filter(event => event.Log && event.Call.Index === call.Call.Index);
 
-
                 eventForCall.forEach(event => {
                     const eventNode = {
                         name: 'Event',
@@ -104,6 +103,7 @@ export default class TreeComponent {
         const ul = this.createElementWithClasses('ul', 'ul-tree')
 
         if (isRoot) ul.id = 'tree';
+        console.log('data.', data)
 
         data.forEach(item => {
             counter++
@@ -114,112 +114,147 @@ export default class TreeComponent {
                 this.updateCollapseButton();
             });
             const summary = this.createElementWithClasses('summary', 'summary-tree', 'card', 'mb-1')
-
             this.appendChildren(details, summary)
             this.appendChildren(li, details)
 
             const contentDiv = this.createElementWithClasses('div')
-
+            const openingDiv = this.createElementWithClasses('span')
+            openingDiv.textContent = '('
+            const closingDiv = this.createElementWithClasses('span')
+            closingDiv.textContent = ')'
+            const pathName = this.createElementWithClasses('span', 'name-tree')
             if (item.name === "Call") {
                 const callDiv = this.createElementWithClasses('div', 'content-tree')
                 if (counter % 2 !== 0) {
                     callDiv.style.boxShadow = 'inset 0 0 0 1000px rgba(0, 0, 0, 0.03)'
                 }
-                const svgElement = this.createSvgWithIcon('call-icon', 'M13 2H18C20.2091 2 22 3.79086 22 6V18C22 20.2091 20.2091 22 18 22H13C10.7909 22 9 20.2091 9 18V16V12.75L15.1893 12.75L13.4697 14.4697C13.1768 14.7626 13.1768 15.2374 13.4697 15.5303C13.7626 15.8232 14.2374 15.8232 14.5303 15.5303L16.8232 13.2374C17.5066 12.554 17.5066 11.446 16.8232 10.7626L14.5303 8.46967C14.2374 8.17678 13.7626 8.17678 13.4697 8.46967C13.1768 8.76256 13.1768 9.23744 13.4697 9.53033L15.1893 11.25L9 11.25V8V6C9 3.79086 10.7909 2 13 2ZM9 11.25H4C3.58579 11.25 3.25 11.5858 3.25 12C3.25 12.4142 3.58579 12.75 4 12.75H9V11.25Z')
+                const iconElement = this.createIcon('fa-share', 'text-primary')
+
                 const method = document.createElement('div');
                 method.textContent = item.call.Signature.Name.length > 1 ? item.call.Signature.Name : item.call.Signature.SignatureHash
+                if (item.call.Signature.SignatureHash === '') method.textContent = 'transfer of money'
                 const gas = this.createElementWithClasses('div')
                 gas.textContent = `Gas: ${item.call.Gas}`
                 const gasUsed = this.createElementWithClasses('div')
                 gasUsed.textContent = `Gas used: ${item.call.GasUsed}`
-                this.appendChildren(callDiv, svgElement, method, gas, gasUsed)
-                const argumentsDiv = this.createElementWithClasses('div', 'event-tree')
 
+                this.appendChildren(callDiv, iconElement, method, gas, gasUsed)
+                const argumentsDiv = this.createElementWithClasses('div', 'event-tree')
+                const callArgumentsDiv = this.createElementWithClasses('div', 'content-tree')
+                const callArgumentsPathDiv = this.createElementWithClasses('div', 'content-tree')
                 item.callArguments.forEach(element => {
-                    const svgElement = this.createSvgWithIcon('argument-icon', 'M7 2.25C3.82436 2.25 1.25 4.82436 1.25 8V20.9194C1.25 22.3868 2.94738 23.2026 4.09322 22.2859L6.92069 20.0239C7.14233 19.8466 7.41772 19.75 7.70156 19.75H17C20.1756 19.75 22.75 17.1756 22.75 14V8C22.75 4.82436 20.1756 2.25 17 2.25H7ZM7.0498 12.2998C7.74016 12.2998 8.2998 11.7402 8.2998 11.0498C8.2998 10.3594 7.74016 9.7998 7.0498 9.7998C6.35945 9.7998 5.7998 10.3594 5.7998 11.0498C5.7998 11.7402 6.35945 12.2998 7.0498 12.2998ZM13.2998 11.0498C13.2998 11.7402 12.7402 12.2998 12.0498 12.2998C11.3594 12.2998 10.7998 11.7402 10.7998 11.0498C10.7998 10.3594 11.3594 9.7998 12.0498 9.7998C12.7402 9.7998 13.2998 10.3594 13.2998 11.0498ZM17.0498 12.2998C17.7402 12.2998 18.2998 11.7402 18.2998 11.0498C18.2998 10.3594 17.7402 9.7998 17.0498 9.7998C16.3594 9.7998 15.7998 10.3594 15.7998 11.0498C15.7998 11.7402 16.3594 12.2998 17.0498 12.2998Z')
                     const block = this.createElementWithClasses('div', 'text-block', 'ml-2')
                     const argName = this.createElementWithClasses('span', 'name-tree')
 
-                    if (element.Type === "address") {
-                        argName.textContent = element.Name.length > 1 ? `${element.Name}:` : `${element.Type}:`
-                        const addressLink = this.config.rendering.renderJustAddressLink(element.Value.address, null, this.chainId)
-                        this.appendChildren(block, svgElement, argName, addressLink)
-                        this.appendChildren(callDiv, block)
-                    }
-                    if (element.Type.startsWith('uint')) {
-                        argName.textContent = element.Name.length > 1 ? `${element.Name}:` : `${element.Type}:`
-                        let addressNumber = this.config.rendering.renderNumbers(element.Value.bigInteger)
-                        this.appendChildren(block, svgElement, argName, addressNumber)
-                        this.appendChildren(callDiv, block)
-                    }
-                    if (element.Type.startsWith('bytes')) {
-                        argName.textContent = element.Name.length > 1 ? `${element.Name}:` : `${element.Type}:`
-                        const valueBytes = this.config.rendering.renderBytes32(element.Value.hex)
-                        this.appendChildren(block, svgElement, argName, valueBytes)
-                        this.appendChildren(callDiv, block)
+                    if (element.Path.length > 0) {
+
+                        pathName.textContent = `${element.Path[0].Name}:`
+
+                        element.Path.forEach(item => {
+                            console.log('item.Name', item.Name)
+                        })
+                        if (element.Type === "address") {
+                            argName.textContent = `${element.Type}:`
+                            const addressLink = this.config.rendering.renderJustAddressLink(element.Value.address, null, this.chainId)
+
+                            this.appendChildren(block, argName, addressLink)
+                            this.appendChildren(callArgumentsPathDiv, block)
+                        }
+                        if (element.Type.startsWith('uint')) {
+                            argName.textContent = `${element.Type}:`
+                            let addressNumber = this.config.rendering.renderNumbers(element.Value.bigInteger)
+
+                            this.appendChildren(block, argName, addressNumber)
+                            this.appendChildren(callArgumentsPathDiv, block)
+                        }
+                        if (element.Type.startsWith('bytes')) {
+                            argName.textContent = `${element.Type}:`
+                            const valueBytes = this.config.rendering.renderBytes32(element.Value.hex)
+
+                            this.appendChildren(block, argName, valueBytes)
+                            this.appendChildren(callArgumentsPathDiv, block)
+                        }
+
+                    } else {
+                        if (element.Type === "address") {
+                            argName.textContent = element.Name.length > 1 ? `${element.Name}:` : `${element.Type}:`
+                            const addressLink = this.config.rendering.renderJustAddressLink(element.Value.address, null, this.chainId)
+
+                            this.appendChildren(block, argName, addressLink)
+                            this.appendChildren(callArgumentsDiv, block)
+                        }
+                        if (element.Type.startsWith('uint')) {
+                            argName.textContent = element.Name.length > 1 ? `${element.Name}:` : `${element.Type}:`
+                            let addressNumber = this.config.rendering.renderNumbers(element.Value.bigInteger)
+
+                            this.appendChildren(block, argName, addressNumber)
+                            this.appendChildren(callArgumentsDiv, block)
+                        }
+                        if (element.Type.startsWith('bytes')) {
+                            argName.textContent = element.Name.length > 1 ? `${element.Name}:` : `${element.Type}:`
+                            const valueBytes = this.config.rendering.renderBytes32(element.Value.hex)
+
+                            this.appendChildren(block, argName, valueBytes)
+                            this.appendChildren(callArgumentsDiv, block)
+                        }
                     }
                 })
-                // if (item.returns.length > 0) {
-                //     const returnContent = document.createElement('div')
-                //     returnContent.classList.add('d-flex')
-                //     returnContent.style.gap = '0 6px'
-                //     const returnContentText = document.createElement('div')
-                //     returnContent.classList.add('name-tree')
-                //     returnContent.textContent = 'Return:'
-                //     item.returns.forEach(element => {
-                //         if (element.Type === "address") {
-                //             const block = document.createElement('div')
-                //             block.classList.add('text-block', 'ml-2')
-                //             let argName = document.createElement('span')
-                //             argName.classList.add('name-tree')
-                //             let addressLink = this.config.rendering.renderJustAddressLink(element.Value.address, null, this.chainId)
-                //             argName.textContent = element.Name ? `${element.Name}:` : `${element.Type}:`
-                //
-                //             block.appendChild(argName)
-                //             block.appendChild(addressLink)
-                //             returnContent.appendChild(block)
-                //         }
-                //         if (element.Type.startsWith('uint')) {
-                //             const block = document.createElement('div')
-                //             block.classList.add('text-block', 'ml-2')
-                //
-                //             let argName = document.createElement('span')
-                //             argName.classList.add('name-tree')
-                //             argName.textContent = element.Name ? `${element.Name}:` : `${element.Type}:`
-                //             let addressNumber = this.config.rendering.renderNumbers(element.Value.bigInteger)
-                //             block.appendChild(argName)
-                //             block.appendChild(addressNumber)
-                //             returnContent.appendChild(block)
-                //         }
-                //         if (element.Type.startsWith('bytes')) {
-                //             const block = document.createElement('div')
-                //             block.classList.add('text-block', 'ml-2')
-                //
-                //             let argName = document.createElement('span')
-                //             argName.classList.add('name-tree')
-                //             argName.textContent = element.Name ? `${element.Name}:` : `${element.Type}:`
-                //             let valueBytes = this.config.rendering.renderBytes32(element.Value.hex)
-                //             block.appendChild(argName)
-                //             block.appendChild(valueBytes)
-                //             returnContent.appendChild(block)
-                //         }
-                //         if (element.Type === "bool") {
-                //             const block = document.createElement('div')
-                //             block.classList.add('text-block', 'ml-2')
-                //
-                //             let argName = document.createElement('span')
-                //             argName.classList.add('name-tree')
-                //             argName.textContent = element.Name ? `${element.Name}:` : `${element.Type}:`
-                //             let valueBytes = document.createElement('span')
-                //             valueBytes.textContent = element.Value.bool
-                //             block.appendChild(argName)
-                //             block.appendChild(valueBytes)
-                //             returnContent.appendChild(block)
-                //         }
-                //     })
-                //     callDiv.appendChild(returnContent)
-                // }
-                // callDiv.appendChild(argumentsDiv);
+                if (item.callArguments.length > 0) {
+                    if (item.callArguments.some(e => e.Path.length > 0)) {
+                        callArgumentsPathDiv.insertBefore(openingDiv, callArgumentsPathDiv.firstChild);
+                        callArgumentsPathDiv.appendChild(closingDiv);
+                        callArgumentsPathDiv.insertBefore(pathName, callArgumentsPathDiv.firstChild);
+                        this.appendChildren(callDiv, callArgumentsPathDiv);
+                    }
+                    this.appendChildren(callDiv, callArgumentsDiv);
+                }
+
+                if (item.returns.length > 0) {
+                    const iconElement = this.createIcon('fa-undo', 'text-danger')
+
+                    const returnContent = this.createElementWithClasses('div', 'content-tree')
+                    returnContent.style.gap = '0 5px'
+                    returnContent.textContent = '[ Return:'
+                    const openingDiv = this.createElementWithClasses('div')
+                    openingDiv.textContent = '['
+                    const closingDiv = this.createElementWithClasses('div')
+                    closingDiv.textContent = ']'
+                    item.returns.forEach(element => {
+                        const block = this.createElementWithClasses('div', 'text-block', 'ml-2')
+                        const argName = this.createElementWithClasses('span', 'name-tree')
+
+                        if (element.Type === "address") {
+                            argName.textContent = element.Name ? `${element.Name}:` : `${element.Type}:`
+                            const addressLink = this.config.rendering.renderJustAddressLink(element.Value.address, null, this.chainId)
+                            this.appendChildren(block, argName, addressLink, closingDiv)
+                        }
+                        if (element.Type.startsWith('uint')) {
+                            argName.textContent = element.Name ? `${element.Name}:` : `${element.Type}:`
+                            let addressNumber = this.config.rendering.renderNumbers(element.Value.bigInteger)
+                            this.appendChildren(block, argName, addressNumber, closingDiv)
+
+                        }
+                        if (element.Type.startsWith('bytes')) {
+                            argName.textContent = element.Name ? `${element.Name}:` : `${element.Type}:`
+                            let valueBytes = this.config.rendering.renderBytes32(element.Value.hex)
+                            this.appendChildren(block, argName, valueBytes, closingDiv)
+
+                        }
+                        if (element.Type === "bool") {
+                            argName.textContent = element.Name ? `${element.Name}:` : `${element.Type}:`
+                            let valueBytes = document.createElement('span')
+                            valueBytes.textContent = element.Value.bool
+                            this.appendChildren(block, argName, valueBytes, closingDiv)
+                        }
+                        this.appendChildren(returnContent, block)
+
+                    })
+                    if (item.returns.length > 0) {
+                        returnContent.insertBefore(iconElement, returnContent.firstChild);
+                        this.appendChildren(callDiv, returnContent)
+                    }
+                }
+                callDiv.appendChild(argumentsDiv);
                 contentDiv.appendChild(callDiv);
 
             }
@@ -228,11 +263,11 @@ export default class TreeComponent {
                 if (counter % 2 !== 0) {
                     argumentsDiv.style.boxShadow = "inset 0 0 0 1000px rgba(0, 0, 0, 0.03)";
                 }
-                const svgElement = this.createSvgWithIcon('event-icon', 'M10.9718 2.70846C11.4382 1.93348 12.5618 1.93348 13.0282 2.70847L15.3586 6.58087C15.5262 6.85928 15.7995 7.05784 16.116 7.13116L20.5191 8.15091C21.4002 8.35499 21.7474 9.42356 21.1545 10.1066L18.1918 13.5196C17.9788 13.765 17.8744 14.0863 17.9025 14.41L18.2932 18.9127C18.3714 19.8138 17.4625 20.4742 16.6296 20.1214L12.4681 18.3583C12.1689 18.2316 11.8311 18.2316 11.5319 18.3583L7.37038 20.1214C6.53754 20.4742 5.62856 19.8138 5.70677 18.9127L6.09754 14.41C6.12563 14.0863 6.02124 13.765 5.80823 13.5196L2.8455 10.1066C2.25257 9.42356 2.59977 8.35499 3.48095 8.15091L7.88397 7.13116C8.20053 7.05784 8.47383 6.85928 8.64138 6.58087L10.9718 2.70846Z')
+                const iconElement = this.createIcon('fa-bolt', 'text-warning')
 
-                argumentsDiv.appendChild(svgElement)
+                argumentsDiv.appendChild(iconElement)
 
-                item.arguments.forEach(element => {
+                item.eventArguments.forEach(element => {
                     const block = this.createElementWithClasses('div', 'text-block', 'ml-2')
                     const argName = this.createElementWithClasses('span', 'name-tree')
                     argName.textContent = element.Name.length > 1 ? `${element.Name}:` : `${element.Type}:`
@@ -256,14 +291,15 @@ export default class TreeComponent {
                 })
                 contentDiv.appendChild(argumentsDiv);
             }
-            console.log('item.children', item)
-            console.log('item.children typeof', typeof item)
+
             if (item.children && item.children.length > 0) {
                 const childUl = this.createTree(item.children, false, counter);
                 details.append(childUl);
             } else {
                 summary.classList.add('no-children');
             }
+
+
             this.appendChildren(summary, contentDiv)
             this.appendChildren(ul, li)
         });
@@ -298,13 +334,11 @@ export default class TreeComponent {
         }
     }
 
-    createSvgWithIcon(svgClass, pathData) {
-        const svgElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        svgElement.classList.add(svgClass);
-        svgElement.setAttribute("viewBox", "0 0 24 24");
-        const pathElement = document.createElementNS("http://www.w3.org/2000/svg", "path");
-        pathElement.setAttribute("d", pathData);
-        svgElement.appendChild(pathElement);
+    createIcon(...classes) {
+        const icon = document.createElement('i')
+        icon.classList.add('fa', 'ml-2', ...classes)
+        icon.style.width = '1rem'
+        return icon
     }
 
     createElementWithClasses(elementType, ...classes) {
