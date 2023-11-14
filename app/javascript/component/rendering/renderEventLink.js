511926559a6fcb200c275cc99397eac298a6f9ec
Nadia <nadia.olerinskaia@gmail.com>
2023-11-14 13:57:52 +0200

    fix links for ide

--- app/javascript/component/rendering/renderEventLink.js
+++ app/javascript/component/rendering/renderEventLink.js
@@ -4,8 +4,8 @@ export default function renderEventLink(data, variables, chainId) {
 	const link = document.createElement('a');
     link.setAttribute('title',data.event)
 
-    // link.href = `https://explorer.bitquery.io/${WidgetConfig.getNetwork(chainId)}/event/${data.hash}`;
-    link.href = `/${WidgetConfig.getNetwork(chainId)}/event/${data.hash}`;
+    link.href = `https://explorer.bitquery.io/${WidgetConfig.getNetwork(chainId)}/event/${data.hash}`;
+    // link.href = `/${WidgetConfig.getNetwork(chainId)}/event/${data.hash}`;
     link.textContent = data.event || data.hash
 
 	div.appendChild(link)
