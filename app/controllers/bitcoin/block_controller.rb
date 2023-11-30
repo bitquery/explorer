d745f9280be0c6de8ea75c12a04b51941fc8de7c
Nadia <nadia.olerinskaia@gmail.com>
2023-11-30 08:46:34 +0200

    authorization test

--- app/controllers/bitcoin/block_controller.rb
+++ app/controllers/bitcoin/block_controller.rb
@@ -16,10 +16,10 @@ class Bitcoin::BlockController < NetworkController
                                                                    network: @network[:network] }, context: { authorization: @streaming_access_token }).data.bitcoin.blocks[0].date.date
 
     if @height.to_i == 666666
-      BitqueryLogger.debug "Secret debug message", height: @height.to_i
-      BitqueryLogger.info "Secret info message", height: @height.to_i
-      BitqueryLogger.warn "Secret warn message", height: @height.to_i
-      BitqueryLogger.error "Secret error message", height: @height.to_i
+      BitqueryLogger.debug 'Secret debug message', height: @height.to_i
+      BitqueryLogger.info 'Secret info message', height: @height.to_i
+      BitqueryLogger.warn 'Secret warn message', height: @height.to_i
+      BitqueryLogger.error 'Secret error message', height: @height.to_i
     end
   end
 end
