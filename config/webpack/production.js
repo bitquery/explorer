0fd84f4c63461d960b87af8968e5bc611ab2ca83
Nadia <nadia.olerinskaia@gmail.com>
2024-12-19 14:54:59 +0200

    error search

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,19 +1,6 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
-const TerserPlugin = require('terser-webpack-plugin')
-
-environment.config.optimization = {
-    minimize: true,
-    minimizer: [
-        new TerserPlugin({
-            terserOptions: {
-                mangle: false,
-                keep_classnames: true,
-                keep_fnames: true,
-            },
-        }),
-    ],
-}
+environment.config.optimization.minimize = false;
 
 module.exports = environment.toWebpackConfig()
