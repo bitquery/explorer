b87d937db306ce131d70b47ae29e469c53e4a5b9
Nadia <nadia.olerinskaia@gmail.com>
2024-12-20 10:33:00 +0200

    error search

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,20 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
-const environment = require('./environment')
+const { webpackConfig, merge } = require("shakapacker");
+const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = environment.toWebpackConfig()
+module.exports = merge(webpackConfig, {
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                exclude: /.*[a-zA-Z]+Component[.]js$/gm,
+                terserOptions: {
+                    mangle: false,
+                    keep_classnames: true,
+                    keep_fnames: true
+                }
+            })
+        ]
+    }
+});
