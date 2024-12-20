79a803e400761b3f835500b838e376e651e01573
Nadia <nadia.olerinskaia@gmail.com>
2024-12-20 13:56:38 +0200

    error search

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,11 +1,11 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'production';
-const config = require('./environment');
-const merge = require("webpack-merge");
-const {baseConfig} = require("shakapacker");
-module.exports = merge(baseConfig, {
+process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+
+const baseConfig = require('./environment')
+
+module.exports = {
+    ...baseConfig,
     mode: 'none',
     optimization: {
         minimize: false
     }
-});
-
+}
