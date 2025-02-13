0b78a7c874f2fa1c6a7c989fa35365cc0f7f1bbd
Nadia <nadia.olerinskaia@gmail.com>
2025-02-13 14:24:48 +0200

    fix network (ethereum, arbitrum)

--- app/javascript/component/rendering/renderPairLink.js
+++ app/javascript/component/rendering/renderPairLink.js
@@ -1,14 +1,14 @@
 export default function renderPairLink(data, variables, chainId) {
-    const div = document.createElement('div');
-    div.classList.add('text-truncate'); // Добавляем класс для стилизации
-    div.style.whiteSpace = 'nowrap';   // Запрещаем перенос строк
-    div.style.overflow = 'hidden';     // Скрываем переполнение
-    div.style.textOverflow = 'ellipsis'; // Добавляем троеточие
+    const div = document.createElement('div')
+    div.classList.add('text-truncate')
+    div.style.whiteSpace = 'nowrap'
+    div.style.overflow = 'hidden'
+    div.style.textOverflow = 'ellipsis'
 
-    const link = document.createElement('a');
-    link.textContent = `${data.buyCurrencySymbol} / ${data.sellCurrencySymbol}`;
-    link.setAttribute('title', `${data.buyCurrencySymbol} / ${data.sellCurrencySymbol}`); // Подсказка при наведении
-    link.href = `/${WidgetConfig.getNetwork(chainId)}/tokenpair/${data.buyCurrencySC}/${data.sellCurrencySC}`;
-    div.appendChild(link);
-    return div;
+    const link = document.createElement('a')
+    link.textContent = `${data.buyCurrencySymbol || data.buyCurrencySC.slice(0,7)+'...'} / ${data.sellCurrencySymbol || data.sellCurrencySC.slice(0,7)+'...'}`
+    link.setAttribute('title', `${data.buyCurrencySymbol || data.buyCurrencySC} / ${data.sellCurrencySymbol || data.sellCurrencySC}`)
+    link.href = `/${WidgetConfig.getNetwork(chainId)}/tokenpair/${data.buyCurrencySC}/${data.sellCurrencySC}`
+    div.appendChild(link)
+    return div
 }
