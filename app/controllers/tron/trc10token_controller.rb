5a3591ff06c9607670b6eecc558fae9ee5e49188
Nadia <nadia.olerinskaia@gmail.com>
2025-05-21 15:58:22 +0300

    fix fiery for tron currency

--- app/controllers/tron/trc10token_controller.rb
+++ app/controllers/tron/trc10token_controller.rb
@@ -16,6 +16,7 @@ module Tron
                             tokenId
                             tokenType
                           }
+                count
                       }
                     }
                   }
